960f2924ff4fb571a5199642e631ea0688fd5d51
rafalafel21 <rosen.raphie@gmail.com>
2020-08-13 20:31:09 -0400

    gradient another try

--- animate/src/countyColor.js
+++ animate/src/countyColor.js
@@ -33,12 +33,47 @@ function countyColor(day, countyid, handleChange) {
 
   const caseDensity = (casesToday - casesYesterday) / population * 100 * 1000;
 
+// As case density goes down, r, g, and b, go up/down by a certain amount. Convert that rgb to hex and return.
+//if 10<caseDensity<25, 25-caseDensity/15
+
+    var colorPercentage;
+    var r = 255;
+    var g = 0;
+    var b = 0;
+    if (caseDensity > 10) {
+        colorPercentage =  (25 - caseDensity)/15;
+        g = colorPercentage * 196;
+        fillColor = "rgb(" + r + "," + g + "," + b + ")";
+    } else if (caseDensity <=10 && caseDensity>1) {
+        r = 255;
+        g = 255;
+        b = 0;
+        colorPercentage = caseDensity/10;
+        r = r - colorPercentage * 255;
+        fillColor = "rgb(" + r + "," + g + "," + b + ")";
+    } else if (caseDensity <=1) {
+        r = 255;
+        g = 255;
+        b = 0;
+        fillColor = "#00FF00"
+    }
+
+
+    /*
   if (caseDensity > 25) {
+<<<<<<< HEAD
+    fillColor = rgb(255,0,0); //red
+  }
+  else if (caseDensity > 20) {
+    fillColor = '#ff8000'; //dark orange
+  }
+=======
     fillColor = '#FF0000'; //red
   }
   else if (caseDensity > 20) {
     fillColor = '#ff8000'; //dark orange
   }
+>>>>>>> fb4f57b495fc3d23eb9596f3b26cd657bc4b0a9f
   else if (caseDensity > 15) {
     fillColor = '#ff9500'; //orange
   }
@@ -50,10 +85,15 @@ function countyColor(day, countyid, handleChange) {
   }
   else if (caseDensity >= 1) {
     fillColor = '#a6ff00'; //light green
+<<<<<<< HEAD
   }
+=======
+  }
+>>>>>>> fb4f57b495fc3d23eb9596f3b26cd657bc4b0a9f
   else if (caseDensity < 1) {
     fillColor = '#00FF00'; //green
   };
+  */
 
   return fillColor;
 };
