bb2d2f5e1960a4bf50c38d5ec6353f7c403911d7
Leo Jacoby <leojacoby@gmail.com>
2020-08-22 14:47:31 -0700

    Screen recording set up with week increments. Put some CAN design in as well

--- animate/src/USCountyMap.js
+++ animate/src/USCountyMap.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import styled from 'styled-components';
+import './Map.css';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
-import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from "react-component-export-image";
-import GIFEncoder from 'gif-encoder-2';
 import countyColor from './countyColor.js';
-import stateColor from './stateColor.js';
 import dayToDate from './dayToDate.js';
-import fs from 'fs';
 import path from 'path';
-
 import COUNTIES_JSON from './data/counties-10m.json';
-import STATES_JSON from './data/states-10m.json';
-import STATE_ABBRV from './data/stateAbbrv.json';
-
-const { createCanvas } = require('canvas');
-
-// const ColorFadeCounty = styled.g`
-//   fill: ${(props) => props.color};
-//   transition: fill 1s ease-in;
-//   onExited
-// `;
-
-
 
 class County extends React.Component {
   constructor(props) {
@@ -32,14 +14,12 @@ class County extends React.Component {
   }
   render() {
     return (
-      // <ColorFadeCounty color={countyColor(this.props.day, this.props.geo.id)}>
       <Geography
       key={this.props.geo.rsmKey}
       geography={this.props.geo}
       fill={countyColor(this.props.day, this.props.geo.id, this.props.handleChange)}
       strokeWidth={.5}
       />
-      // </ColorFadeCounty>
   )
   }
 }
@@ -47,65 +27,30 @@ class County extends React.Component {
 class USCountyMap extends React.Component {
   constructor(props) {
     super(props);
-    this.encoder = new GIFEncoder(600, 400);
-    this.encoder.setDelay(500);
-    this.encoder.start();
-    this.componentRef = React.createRef();
+
     this.state = {day: 1, date: ''}
   }
 
-  // clickHandler() {
-  //   const day = this.state.day;
-  //
-  //   if (day < 125) {
-  //     //exportComponentAsPNG(this.componentRef);
-  //     //console.log("printing component for day", day);
-  //     this.setState({day: this.state.day + 1})
-  //   }
-  // }
 
   componentDidMount() {
-    // if (this.state.day < 125) {
-    //   this.encoder.addFrame(this.componentRef);
-    //   this.setState({day: this.state.day + 1});
-    // } else {
-    //   this.encoder.finish();
-    //   const buffer = this.encoder.out.getData();
-    //
-    //   writeFile(path.join(__dirname, 'output', 'map.gif'), buffer, error => {
-    //     console.log("buffer", buffer);
-    //     console.log("error", error);
-    //   });
-    // }
-
-
     this.myInterval = setInterval(() => {
       const day = this.state.day;
-
-      if (day < 5) {
-        this.encoder.addFrame(this.componentRef);
-
-        this.setState({day: this.state.day + 1})
+      if (day < 175) {
+        this.setState({day: this.state.day + 7})
       } else {
-        this.encoder.finish();
-        const buffer = this.encoder.out.getData();
-
-        fs.writeFile(path.join(__dirname, 'output', 'map.gif'), buffer, error => {
-          console.log("buffer", buffer);
-          console.log("error", error);
-        });
         clearInterval(this.myInterval);
       }
     }, 10)
   }
 
   render() {
+
     return (
-    //<React.Fragment>
     <div ref={this.componentRef}>
-    <h1 style={{fontSize:5 + 'em'}}>
-    {dayToDate(this.state.day).substring(5,7) + '/' + dayToDate(this.state.day).substring(8,10) + '/20'}
+    <h1 className="header">
+    New cases per 100k people
     </h1>
+    <img src={process.env.PUBLIC_URL + "/color-key.png"} />
     <ComposableMap data-tip="" projection="geoAlbersUsa" stroke={'white'} style={{width: 80+'%'}}>
         <Geographies geography={COUNTIES_JSON}>
           {({ geographies }) =>
@@ -117,62 +62,10 @@ class USCountyMap extends React.Component {
           }
         </Geographies>
       </ComposableMap>
+      <img src={process.env.PUBLIC_URL + "/can-logo.png"} />
       </div>
-      //</React.Fragment>
     )
   }
 }
 
-// class USCountyMap extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.componentRef = React.createRef();
-//   }
-//
-//   componentDidMount() {
-//     exportComponentAsPNG(this.componentRef);
-//   }
-//
-//   render() {
-//
-//     return (
-//       <React.Fragment>
-//         <MapToPrint ref={this.componentRef} />
-//        //  <button onClick={() => exportComponentAsPNG(this.componentRef)}>
-//        //     Export As PNG
-//        // </button>
-//       </React.Fragment>
-//     );
-//   }
-// }
-//
 export default USCountyMap;
-
-
-// class ComponentToPrint extends React.Component {
-//  render() {
-//    return <div>Hello World</div>;
-//  }
-// }
-// export default class USCountyMap extends React.Component {
-//  constructor(props) {
-//    super(props);
-//    this.componentRef = React.createRef();
-//  }
-//
-//  render() {
-//    return (
-//    <React.Fragment>
-//        <ComponentToPrint ref={this.componentRef} />
-//        <button onClick={() => exportComponentAsJPEG(this.componentRef)}>
-//            Export As JPEG
-//        </button>
-//        <button onClick={() => exportComponentAsPDF(this.componentRef)}>
-//            Export As PDF
-//        </button>
-//        <button onClick={() => exportComponentAsPNG(this.componentRef)}>
-//            Export As PNG
-//        </button>
-//    </React.Fragment>);
-//  }
-// }
--- animate/src/countyColor.js
+++ animate/src/countyColor.js
@@ -6,8 +6,6 @@ import tinygradient from 'tinygradient';
 function countyColor(day, countyid, handleChange) {
   var fillColor;
   const trailingDates = [dayToDate(day-7), dayToDate(day-6), dayToDate(day-5), dayToDate(day-4), dayToDate(day-3), dayToDate(day-2), dayToDate(day-1), dayToDate(day)]
-  // const today = dayToDate(day);
-  // const yesterday = dayToDate(day - 1);
 
   var dataForDate;
   var dataForCounty;
@@ -22,12 +20,10 @@ function countyColor(day, countyid, handleChange) {
     if (!dataForCounty) {
       return null;
     }
-    // population = dataForCounty['population'];
     let caseDensity = dataForCounty['caseDensity'];
     return caseDensity;
   })
 
-  // console.log('newCaseDensityArray', newCaseDensityArray);
 
   newCaseDensityArray = newCaseDensityArray.filter(x => x!== null);
   // if (newCaseDensityArray.length < 5) {
@@ -35,34 +31,6 @@ function countyColor(day, countyid, handleChange) {
   // }
   const newCaseDensityTrailingAverage = newCaseDensityArray.reduce((a,b) => a+b, 0) / newCaseDensityArray.length;
 
-  // console.log('newCaseDensityTrailingAverage', newCaseDensityTrailingAverage);
-
-
-  // const dataForDate = COUNTY_DATA[today];
-  // if (!dataForDate) {
-  //   return 'gray'
-  // };
-  // const dataForCounty = dataForDate[countyid];
-  // if (!dataForCounty) {
-  //   return 'gray';
-  // };
-  // const population = dataForCounty['population'];
-  //
-  // let casesToday = dataForCounty['cumulativeConfirmedCases'];
-  // if (casesToday === null) {
-  //   return '#00d475';
-  // };
-  //
-  // let dataForDateYesterday = COUNTY_DATA[yesterday]
-  // if (!dataForDateYesterday) {
-  //   var casesYesterday = 0
-  // } else {
-  //   var dataForCountyYesterday = dataForDateYesterday[countyid];
-  //   var casesYesterday = dataForCountyYesterday['cumulativeConfirmedCases'];
-  //   casesYesterday = casesYesterday ? casesYesterday : 0;
-  // }
-  //
-  // const caseDensity = (casesToday - casesYesterday) / population * 100 * 1000;
 
   var gradient = tinygradient([
     {color: '#00d474', pos: 0},
