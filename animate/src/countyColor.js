c7df362857b1a1ed6a944bafaa392520f89e0381
Benjamin Wu <benjiwu@Benjamins-MacBook-Air-2.local>
2020-10-16 12:29:29 -0700

    new data

--- animate/src/countyColor.js
+++ animate/src/countyColor.js
@@ -1,4 +1,4 @@
-import COUNTY_DATA from './data/county-history-data.json';
+import COUNTY_DATA from './data/county-history-data3.json';
 import dayToDate from './dayToDate.js';
 import tinygradient from 'tinygradient';
 
